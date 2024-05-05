4402b60664ea56ec7847ef1b5da8b387c6bc0c2f
Zarinjahanshazi <zarinshazi@gmail.com>
2024-05-05 20:43:51 +0600

    my code

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const express = require('express');
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
 
 //middleWere
-app.use(cors());
+app.use(cors({
+    origin:['http://localhost:5173'],
+    credentials:true
+}));
 app.use(express.json());
+app.use(cookieParser());
 
 
 console.log(process.env.DB_PASS)
@@ -23,13 +29,63 @@ const client = new MongoClient(uri, {
   }
 });
 
+//middleWares
+const logger = async(req, res,next) =>{
+    console.log('called',req.host,req.originalUrl)
+    next();
+}
+
+const verifyToken = async(req,res,next) =>{
+    const token = req.cookies?.token;
+    console.log('value of token in middleware',token)
+    if(!token){
+        return res.status(401).send({message:'not authorized'})
+    }
+    jwt.verify(token,process.env.ACCESS_TOKEN_SECRET,(err,decoded) =>{
+        //error
+        if(err){
+            console.log(err);
+            return res.status(401).send({message:'unauthorized Access'})
+        }
+        //if token is valid then it would be decoded
+        console.log('value in the token',decoded)
+        req.user=decoded
+
+        
+        next()
+    })
+
+}
+
+
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     const serviceCollection = client.db('carDoctor').collection('services');
     const bookingCollection = client.db('carDoctor').collection('bookings');
-    app.get('/services',async(req,res) =>{
+    
+    //Auth related API
+    app.post('/jwt',logger,  async(req,res) =>{
+        const user = req.body;
+        console.log(user);
+        const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET,{expiresIn: '1h'})
+
+        res
+        .cookie('token',token,{
+            httpOnly: true,
+
+            //deploy korar shomoy uncomment kore nite hobe
+            secure:false,
+            // sameSite:'none'
+        })
+        .send({success:true});
+    })
+    
+    
+    //services related API
+    app.get('/services',logger, async(req,res) =>{
         const cursor = serviceCollection.find();
         const result = await cursor.toArray();
         res.send(result);
@@ -49,8 +105,13 @@ async function run() {
     })
 
     //bookings
-    app.get('/bookings',async(req,res) =>{
+    app.get('/bookings',logger,verifyToken, async(req,res) =>{
         console.log(req.query.email);
+        console.log('user in the valid token',req.user)
+        // console.log('tok tok token',req.cookies.token)
+        if(req.query.email !== req.user.email){
+            return res.status(403).send({message:'forbidden Access'})
+        }
         let query={};
         if( req.query?.email){
             query = { email:req.query.email}
